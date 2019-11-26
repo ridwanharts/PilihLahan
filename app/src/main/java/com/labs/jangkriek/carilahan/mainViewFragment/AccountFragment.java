299cd5809c02436e5130088d414f693bb4fac5e8
ridwanharts <twentyridwan@gmail.com>
2019-11-26 14:53:55 +0700

    selesai image lahan

--- app/src/main/java/com/labs/jangkriek/carilahan/mainViewFragment/AccountFragment.java
+++ app/src/main/java/com/labs/jangkriek/carilahan/mainViewFragment/AccountFragment.java
@@ -15,6 +15,7 @@ import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.Toast;
 
@@ -50,19 +51,17 @@ import static com.mapbox.mapboxsdk.style.layers.PropertyFactory.lineWidth;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class AccountFragment extends Fragment implements OnMapReadyCallback, MapboxMap.OnMapClickListener, MapboxMap.OnCameraIdleListener {
+public class AccountFragment extends Fragment implements OnMapReadyCallback, MapboxMap.OnMapClickListener {
 
 
-    private MapView mMapView;
+    /*private MapView mMapView;
     private MapboxMap mMapboxMap;
     private FeatureCollection featureCollection;
-
     private FloatingActionButton setStyle, showArea, fabLogOut;
-
     private GeoJsonSource geoJsonSource;
     private LatLng currentPos = new LatLng(-7.905, 110.06);
-
-    private String currentMap;
+    private String currentMap;*/
+    private Button btnLogout;
 
     public AccountFragment() {
         // Required empty public constructor
@@ -74,13 +73,14 @@ public class AccountFragment extends Fragment implements OnMapReadyCallback, Map
         View view = inflater.inflate(R.layout.fragment_account, container, false);
 
         // Inflate the layout for this fragment
-        mMapView = view.findViewById(R.id.map_view3);
+/*        mMapView = view.findViewById(R.id.map_view3);
         mMapView.onCreate(savedInstanceState);
         mMapView.getMapAsync(this);
 
         setStyle = view.findViewById(R.id.set_style);
         showArea = view.findViewById(R.id.show_area);
-        fabLogOut = view.findViewById(R.id.logout);
+        fabLogOut = view.findViewById(R.id.logout);*/
+        btnLogout = view.findViewById(R.id.logout);
 
         return view;
     }
@@ -90,7 +90,7 @@ public class AccountFragment extends Fragment implements OnMapReadyCallback, Map
 
         super.onViewCreated(view, savedInstanceState);
 
-        fabLogOut.setOnClickListener(new View.OnClickListener() {
+        btnLogout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(getActivity(), LoginActivity.class);
@@ -104,7 +104,7 @@ public class AccountFragment extends Fragment implements OnMapReadyCallback, Map
 
     @Override
     public void onMapReady(@NonNull MapboxMap mapboxMap) {
-        this.mMapboxMap = mapboxMap;
+        /*this.mMapboxMap = mapboxMap;
         geoJsonSource = new GeoJsonSource("source-main",
                 Feature.fromGeometry(Point.fromLngLat(currentPos.getLongitude(),
                         currentPos.getLatitude())));
@@ -149,7 +149,7 @@ public class AccountFragment extends Fragment implements OnMapReadyCallback, Map
             }
         });
 
-
+*/
     }
 
     @Override
@@ -161,7 +161,7 @@ public class AccountFragment extends Fragment implements OnMapReadyCallback, Map
     /**
      * Sets up the source and layer for drawing the building outline
      */
-    private void setUpLineLayer(@NonNull Style loadedMapStyle) {
+    /*private void setUpLineLayer(@NonNull Style loadedMapStyle) {
         // Create a GeoJSONSource from an empty FeatureCollection
         loadedMapStyle.addSource(new GeoJsonSource("source",
                 FeatureCollection.fromFeatures(new Feature[]{})));
@@ -173,124 +173,49 @@ public class AccountFragment extends Fragment implements OnMapReadyCallback, Map
                 lineCap(LINE_CAP_ROUND),
                 lineJoin(LINE_JOIN_BEVEL)
         ));
-    }
-
-    @Override
-    public void onCameraIdle() {
-        mMapboxMap.getStyle(new Style.OnStyleLoaded() {
-            @Override
-            public void onStyleLoaded(@NonNull Style style) {
-                updateOutline(style);
-            }
-        });
-    }
-
-    /**
-     * Query the map for a building Feature in the map's building layer. The query happens in the middle of the
-     * map ("the target"). If there's a building Feature in the middle of the map, its coordinates are turned
-     * into a list of Point objects so that a LineString can be created.
-     *
-     * @return the LineString built via the building's coordinates
-     */
-    private LineString getBuildingFeatureOutline(@NonNull Style style) {
-        // Retrieve the middle of the map
-        final PointF pixel = mMapboxMap.getProjection().toScreenLocation(new LatLng(
-                mMapboxMap.getCameraPosition().target.getLatitude(),
-                mMapboxMap.getCameraPosition().target.getLongitude()
-        ));
-
-        List<Point> pointList = new ArrayList<>();
-
-        // Check whether the map style has a building layer
-        if (style.getLayer("batas-kec-temon-line") != null) {
-
-            // Retrieve the building Feature that is displayed in the middle of the map
-            List<Feature> features = mMapboxMap.queryRenderedFeatures(pixel, "batas-kec-temon-line");
-            if (features.size() > 0) {
-                if (features.get(0).geometry() instanceof Polygon) {
-                    Polygon buildingFeature = (Polygon) features.get(0).geometry();
-                    // Build a list of Point objects from the building Feature's coordinates
-                    for (int i = 0; i < buildingFeature.coordinates().size(); i++) {
-                        for (int j = 0;
-                             j < buildingFeature.coordinates().get(i).size(); j++) {
-                            pointList.add(Point.fromLngLat(
-                                    buildingFeature.coordinates().get(i).get(j).longitude(),
-                                    buildingFeature.coordinates().get(i).get(j).latitude()
-                            ));
-                        }
-                    }
-                }
-                // Create a LineString from the list of Point objects
-            }
-        } else {
-            Toast.makeText(getActivity(), R.string.building_layer_not_present, Toast.LENGTH_SHORT).show();
-        }
-        return LineString.fromLngLats(pointList);
-    }
-
-    /**
-     * Update the FeatureCollection used by the building outline LineLayer. Then refresh the map.
-     */
-    private void updateOutline(@NonNull Style style) {
-        // Update the data source used by the building outline LineLayer and refresh the map
-        featureCollection = FeatureCollection.fromFeatures(new Feature[]
-                {Feature.fromGeometry(getBuildingFeatureOutline(style))});
-        GeoJsonSource source = style.getSourceAs("source");
-        if (source != null) {
-            source.setGeoJson(featureCollection);
-        }
-    }
-
-    private void showCrosshair() {
-        View crosshair = new View(getContext());
-        crosshair.setLayoutParams(new FrameLayout.LayoutParams(20, 20, Gravity.CENTER));
-        crosshair.setBackgroundColor(Color.RED);
-        mMapView.addView(crosshair);
-    }
-
-
+    }*/
 
 
     @Override
     public void onStart() {
         super.onStart();
-        mMapView.onStart();
+       // mMapView.onStart();
     }
 
     @Override
     public void onResume() {
         super.onResume();
-        mMapView.onResume();
+        //mMapView.onResume();
     }
 
     @Override
     public void onPause() {
         super.onPause();
-        mMapView.onPause();
+        //mMapView.onPause();
     }
 
     @Override
     public void onStop() {
         super.onStop();
-        mMapView.onStop();
+        //mMapView.onStop();
     }
 
     @Override
     public void onLowMemory() {
         super.onLowMemory();
-        mMapView.onLowMemory();
+        //mMapView.onLowMemory();
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
-        mMapView.onDestroy();
+        //mMapView.onDestroy();
     }
 
     @Override
     public void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        mMapView.onSaveInstanceState(outState);
+        //mMapView.onSaveInstanceState(outState);
     }
 
 }
